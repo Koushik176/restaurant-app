a20cb88e4dea481dc2e1ab137288af6c80aa53e0
Koushik <koushikashok176@gmail.com>
2023-07-16 17:11:06 +0530

    Restuarant summary and Dummy meals

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
